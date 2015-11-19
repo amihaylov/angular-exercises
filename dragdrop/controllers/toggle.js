cf3150b3c9399bdc7fbcd01725b3a417094612bd
Anton Mihaylov <anton.mihaylov7@gmail.com>
2015-11-19 12:24:57 +0200

    Fixed showing info, so now relies on API callback and works as it should.

--- dragdrop/controllers/toggle.js
+++ dragdrop/controllers/toggle.js
@@ -1,9 +1,16 @@
 angular.module("dragDrop").controller("ToggleController", function ($scope) {
     
     $scope.showInfo = false;
+    var previous;
 
-    $scope.showText = function() {
-        $scope.showInfo = !$scope.showInfo;
+    $scope.selectItem = function(item) {
+    	$scope.models.selected = item;
+    	if (!previous || previous !== item.label) {
+    		$scope.showInfo = true;
+    		previous = item.label;
+    	} else {
+        	$scope.showInfo = !$scope.showInfo;
+        }
     };
 
 });
